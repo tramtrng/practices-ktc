3a55994074b63902e6924340855e2404cc68410f
tramtr <trtram2003@gmail.com>
2025-07-09 11:39:35 +0700

    test

--- vite-project/src/components/Homework_W1_D5/Shopping/components/CartContext.tsx
+++ vite-project/src/components/Homework_W1_D5/Shopping/components/CartContext.tsx
@@ -65,6 +65,7 @@ export const CartProvider: React.FC<{ children: ReactNode }> = ({ children }) =>
   );
 };
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useCart = () => {
   const context = useContext(CartContext);
   if (!context) {
